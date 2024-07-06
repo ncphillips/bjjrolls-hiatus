f5f25794d9b328deda73a3e90d1deaec172924a8
nolan <nolan@ncphi.com>
2024-07-06 10:26:40 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="page-block flex flex-col gap-4 max-w-3xl">
+    <h1 class="heading xxl italic">BjjRolls</h1>
+
+    <p class="text-2xl">We're on hiatus...for now.</p>
+
+    <p>
+      <strong>After a year of running BjjRols we've learned a lot.</strong> The
+      value of recording and sharing trainging footage is clear.
+      <strong>Students love it</strong> and get a ton of value from being able
+      to review our training footage. But we've also learned that the process of
+      <strong>recording and uploading footage is too cumbersome</strong>.
+    </p>
+
+    <p>
+      We're working on a new approach that we hope will
+      <strong> make recording and uploading content efffortless. </strong>
+      Until then, we've decided to shut down the site to reduce costs.
+    </p>
+
+    <p>
+      If you have any questions for us, feel free to
+      <strong>
+        message us on Instagram at
+        <a class="link" href="https://www.instagram.com/bjj.rolls">
+          @bjj.rolls
+        </a>
+      </strong>
+    </p>
+  </section>
+</template>
